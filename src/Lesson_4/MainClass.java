1bc5097331bea978abbf7b6dbf3cb7d2b3a6e9bc
sindersGIT <sinder.smirnov@gmail.com>
2018-07-20 21:02:19 +0300

    tt

---
+++ src/Lesson_4/MainClass.java
@@ -0,0 +1,16 @@
+package Lesson_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainClass {
+    public static void main(String[] args) {
+        new Start();
+    }
+}
+
+
+
+
